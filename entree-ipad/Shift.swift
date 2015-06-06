1343ffb4e5e1c692cb49ebae941be90fa37e3381
Jesse <tiptonjesse@gmail.com>
2015-06-06 17:55:22 -0500

    Removed restaurant property from shift model

--- entree-ipad/Shift.swift
+++ entree-ipad/Shift.swift
@@ -3,7 +3,6 @@ class Shift: PFObject, PFSubclassing {
     
     @NSManaged var employee: Employee
     @NSManaged var endedAt: NSDate
-    @NSManaged var restaurant: Restaurant
     @NSManaged var startedAt: NSDate
     
     static func parseClassName() -> String {
