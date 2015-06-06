58a21b51dcfb54b863f2dfd6702bac302fe4b921
Jesse <tiptonjesse@gmail.com>
2015-06-06 15:34:16 -0500

    Added Parse and THPinViewController via cocoapods

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'entree-ipad' do
+
+pod 'Parse'
+pod 'THPinViewController'
+
+end
+
---
+++ Pods/Target Support Files/Pods-entree-ipad-THPinViewController/Pods-entree-ipad-THPinViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_entree_ipad_THPinViewController : NSObject
+@end
+@implementation PodsDummy_Pods_entree_ipad_THPinViewController
+@end
