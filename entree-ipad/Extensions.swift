8c75b528e4fc7fbcf52cb390e47a3a3c75d9f383
Jesse Tipton <jessetipton@icloud.com>
2015-06-15 17:41:58 -0500

    Added colorIndex extension to UIColor

--- entree-ipad/Extensions.swift
+++ entree-ipad/Extensions.swift
@@ -15,4 +15,23 @@ extension UIColor {
         return UIColor(red:0.99, green:0.23, blue:0.25, alpha:1)
     }
     
+    static func entreeColorForIndex(index: Int) -> UIColor {
+        switch index {
+        case 0:
+            return UIColor.redColor()
+        case 1:
+            return UIColor.orangeColor()
+        case 2:
+            return UIColor.yellowColor()
+        case 3:
+            return UIColor.greenColor()
+        case 4:
+            return UIColor.blueColor()
+        case 5:
+            return UIColor.purpleColor()
+        default:
+            return UIColor.blackColor()
+        }
+    }
+    
 }
