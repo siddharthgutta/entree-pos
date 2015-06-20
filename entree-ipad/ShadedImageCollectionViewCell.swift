aaecaca7cbf52519ce4d6534fbe2aea78d588769
Jesse Tipton <me@jessetipton.com>
2015-06-20 02:51:34 -0500

    Changed ShadedImageCollectionView to UICollectionViewCell

--- entree-ipad/ShadedImageCollectionViewCell.swift
+++ entree-ipad/ShadedImageCollectionViewCell.swift
@@ -1,20 +1,20 @@
 
 import UIKit
 
-class ShadedImageCollectionViewCell: PFCollectionViewCell {
+class ShadedImageCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet var shadedImageView: UIImageView!
-    @IBOutlet var shadeView: UIView!
-    @IBOutlet var primaryTextLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var overlayView: UIView!
+    @IBOutlet var textLabel: UILabel!
     @IBOutlet var detailTextLabel: UILabel!
     @IBOutlet var badgeLabel: BadgeLabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        shadedImageView.setTranslatesAutoresizingMaskIntoConstraints(false)
-        shadeView.setTranslatesAutoresizingMaskIntoConstraints(false)
-        primaryTextLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        overlayView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        textLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
         detailTextLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
         badgeLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
     }
