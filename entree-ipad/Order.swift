6c5d1acf7d4a72a0a5d90e051baac80908e4c30c
Jesse Tipton <me@jessetipton.com>
2015-06-22 15:14:38 -0500

    Replaced restaurant with party on Order

--- entree-ipad/Order.swift
+++ entree-ipad/Order.swift
@@ -4,7 +4,7 @@ class Order: PFObject, PFSubclassing {
     @NSManaged var menuItem: MenuItem
     @NSManaged var menuItemModifiers: [MenuItemModifier]
     @NSManaged var notes: String
-    @NSManaged var restaurant: Restaurant
+    @NSManaged var party: Party
     @NSManaged var seat: Int
     
     static func parseClassName() -> String {
