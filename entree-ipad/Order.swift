87e8f02bd0c4b12af1202bd2fc7df712917b93e8
Jesse Tipton <me@jessetipton.com>
2015-07-03 15:02:34 -0500

    Added total price method to order

--- entree-ipad/Order.swift
+++ entree-ipad/Order.swift
@@ -7,6 +7,12 @@ class Order: PFObject, PFSubclassing {
     @NSManaged var party: Party
     @NSManaged var seat: Int
     
+    func totalPrice() -> Double {
+        return menuItemModifiers.reduce(menuItem.price) { (price: Double, modifier: MenuItemModifier) -> Double in
+            return price + modifier.price
+        }
+    }
+    
     static func parseClassName() -> String {
         return "Order"
     }
