c52226c9c83df07d00f283796884f5ef48aaedfa
Jesse Tipton <me@jessetipton.com>
2015-07-06 16:37:40 -0500

    Replaced printJobs with printText for menu item modifiers

--- entree-ipad/MenuItemModifier.swift
+++ entree-ipad/MenuItemModifier.swift
@@ -4,7 +4,7 @@ class MenuItemModifier: PFObject, PFSubclassing {
     @NSManaged var menuItems: PFRelation
     @NSManaged var name: String
     @NSManaged var price: Double
-    @NSManaged var printJobs: [PrintJob]
+    @NSManaged var printText: String
     
     static func parseClassName() -> String {
         return "MenuItemModifier"
