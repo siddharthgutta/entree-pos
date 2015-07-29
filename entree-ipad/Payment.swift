f22176aac438a61876e5a3bdbc036e3542e9d0c6
Jesse Tipton <me@jessetipton.com>
2015-07-28 22:57:04 -1000

    Payment class refactor

--- entree-ipad/Payment.swift
+++ entree-ipad/Payment.swift
@@ -1,15 +1,45 @@
 
 class Payment: PFObject, PFSubclassing {
     
-    @NSManaged var cardFlightChargeToken: String
-    @NSManaged var cardLastFour: String
-    @NSManaged var cardName: String
-    @NSManaged var orders: PFRelation
+    @NSManaged var type: String
+    
+    @NSManaged var authorized: Bool
+    @NSManaged var cardFlightChargeToken: String?
+    @NSManaged var cardLastFour: String?
+    @NSManaged var cardName: String?
+    @NSManaged var paid: Bool
+    
+    @NSManaged var orders: [Order]
+    
     @NSManaged var subtotal: Double
     @NSManaged var tax: Double
+    @NSManaged var total: Double
+    
     @NSManaged var tip: Double
-    @NSManaged var total: Double // (subtotal + tax + tip)
-    @NSManaged var type: String // Either Card or Cash
+    
+    init(type: String, orders: [Order]) {
+        super.init()
+        
+        self.type = type
+        
+        self.authorized = false
+        self.cardFlightChargeToken = nil
+        self.cardLastFour = nil
+        self.cardName = nil
+        self.paid = false
+        
+        self.orders = orders
+        
+        calculateTotal()
+        
+        self.tip = 0
+    }
+    
+    func calculateTotal() {
+        subtotal = orders.reduce(0) { (s: Double, order: Order) -> Double in return s + order.price() }
+        tax = orders.reduce(0) { (t: Double, order: Order) -> Double in return t + order.tax() }
+        total = orders.reduce(0) { (t: Double, order: Order) -> Double in return t + order.total() }
+    }
     
     static func parseClassName() -> String {
         return "Payment"
