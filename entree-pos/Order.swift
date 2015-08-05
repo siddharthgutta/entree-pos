5e0e01d00398d6fa9a33fe1fdac6c25f59d482e1
Jesse Tipton <me@jessetipton.com>
2015-08-05 14:20:37 -0500

    Order class cleaned

--- entree-pos/Order.swift
+++ entree-pos/Order.swift
@@ -1,34 +1,19 @@
 
 class Order: PFObject, PFSubclassing {
     
-    @NSManaged var menuItem: MenuItem
-    @NSManaged var menuItemModifiers: [MenuItemModifier]
-    @NSManaged var notes: String
-    @NSManaged var party: Party
-    @NSManaged var seat: Int
+    @NSManaged var orderItems: [OrderItem]
+    @NSManaged var payment: Payment
     
-    func price() -> Double {
-        return menuItemModifiers.reduce(menuItem.price) { (price: Double, modifier: MenuItemModifier) -> Double in
-            return price + modifier.price
-        }
+    func subtotal() -> Double {
+        return orderItems.reduce(0) { (subtotal: Double, orderItem: OrderItem) -> Double in return subtotal + orderItem.totalCost() }
     }
     
     func tax() -> Double {
-        var orderTax: Double = 0
-        
-        if menuItem.alcoholic {
-            let alcoholTaxRate = NSUserDefaults.standardUserDefaults().objectForKey("alcohol_tax_rate")! as! Double
-            orderTax *= (1 + alcoholTaxRate)
-        }
-        
-        let salesTaxRate = NSUserDefaults.standardUserDefaults().objectForKey("sales_tax_rate")! as! Double
-        orderTax *= (1 + salesTaxRate)
-        
-        return orderTax
+        return orderItems.reduce(0) { (subtotal: Double, orderItem: OrderItem) -> Double in return subtotal + orderItem.tax() }
     }
     
     func total() -> Double {
-        return price() + tax()
+        return subtotal() + tax()
     }
     
     static func parseClassName() -> String {
